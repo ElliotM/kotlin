e9fd046683c26795c69e93cd7cdbe587863b0a4b
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2014-01-22 17:58:48 +0400

    do not try properties named 'invoke' when resolve 'invoke'

--- compiler/tests/org/jetbrains/jet/checkers/JetDiagnosticsTestGenerated.java
+++ compiler/tests/org/jetbrains/jet/checkers/JetDiagnosticsTestGenerated.java
@@ -5973,6 +5973,11 @@ public class JetDiagnosticsTestGenerated extends AbstractJetDiagnosticsTest {
                     doTest("compiler/testData/diagnostics/tests/resolve/invoke/kt4204-completeNestedCallsForInvoke.kt");
                 }
                 
+                @TestMetadata("valNamedInvoke.kt")
+                public void testValNamedInvoke() throws Exception {
+                    doTest("compiler/testData/diagnostics/tests/resolve/invoke/valNamedInvoke.kt");
+                }
+                
             }
             
             @TestMetadata("compiler/testData/diagnostics/tests/resolve/nestedCalls")
